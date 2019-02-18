7b4e8c1a094aa60d7eb214df51f41a2c44a5bd9b
ivan <vanyaslobodnich@gmail.com>
2019-02-18 16:55:13 +0200

    fix route and add history

--- src/App.js
+++ src/App.js
@@ -8,23 +8,12 @@ import Store from './store';
 
 var url = 'https://gist.githubusercontent.com/snownoop/e6ca04705cf03cbe6ef9beaf16a306ab/raw/07906333730ca961a8091a8c16b05d26a8ee7cd9/Tags%2520Cloud%2520Data',
     store = new Store(),
+    history = [],
     pages;
 
 
-function getData(callback) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.open('GET', url, false);
-
-    xhr.send();
-
-    if (xhr.status !== 200) {
-        // console.log(xhr.status + ': ' + xhr.statusText);
-        callback({message: xhr.status + ': ' + xhr.statusText});
-    } else {
-        // console.log(xhr.responseText);
-        callback(null, xhr.responseText);
-    }
+function changeUrl(path) {
+    window.history.pushState(null, null, path);
 }
 
 
@@ -39,21 +28,10 @@ class Link extends Component {
 
 
     route = (link) => {
-        console.log('call link', link);
         let id = this.props.id ? '/#' + this.props.id : '';
 
-
-        // if (link === '/home') {
-        //     window.history.pushState(null, null, '/home');
-        //     ReactDOM.render(pages['/home'].component, document.getElementById('root'));
-        // } else { // add id
-        //     window.history.pushState(null, null, '/home/#1751295897__Odessa');
-        //     ReactDOM.render(pages['/detail'].component, document.getElementById('root'));
-        // }
-
-        if ( link in pages ) {
-            window.history.pushState(null, null, '/home' + id);
-            ReactDOM.render(pages[link].component, document.getElementById('root'));
+        if (link in pages) {
+            ReactDOM.render(pages[link].load('/home' + id), document.getElementById('root'));
         }
     };
 
@@ -68,6 +46,7 @@ class Link extends Component {
     }
 }
 
+
 class Home extends Component {
     constructor(props) {
         super(props);
@@ -78,12 +57,10 @@ class Home extends Component {
 
         return (
             <div>
-                {/*<Link to={'/detail'} key={'Detail'} name={'Detail'}/>*/}
                 <h3>Home</h3>
 
                 {items.map(item => (
                     <Link to={'/home/:id'} id={item.id} key={item.label} name={item.label}/>
-                    // <Link to={'/home/#' + item.id} key={item.label} name={item.label}/>
                 ))}
             </div>
         )
@@ -142,99 +119,38 @@ class Detail extends Component {
     };
 }
 
-// class Home extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             // hash: window.location.hash
-//         };
-//     }
-//
-//
-//     getItemById = (id, items) => {
-//         let index = 0;
-//
-//         for (index; index < items.length; index++) {
-//             console.log((items[index].id === id), items[index].id, id);
-//             if (items[index].id === id) {
-//                 return items[index];
-//             }
-//         }
-//
-//         return null;
-//     };
-//
-//     render() {
-//         let {items} = store.getState(),
-//             hash = window.location.hash,
-//             item, id;
-//
-//         console.log('hash: ' + hash);
-//
-//         if (hash) {
-//             // get rid of hash symbol
-//             id = hash.slice(1);
-//
-//             item = this.getItemById(id, items);
-//
-//             console.log('item: ', item);
-//
-//             if (item) {
-//                 console.log('load detail');
-//                 return <Detail mergeState={store.mergeState.bind(store)} id={id} item={item}/>
-//             }
-//         }
-//
-//         console.log('load home');
-//
-//         return (
-//             <div>
-//                 {items.map(item => (
-//                     <Link to={'/home/#' + item.id} key={item.label} name={item.label}/>
-//                 ))}
-//             </div>
-//         );
-//     }
-// }
-//
-//
-// class Detail extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             id: props.id,
-//             item: props.item
-//         }
-//     }
-//
-//     render() {
-//         let item = this.state.item;
-//
-//         return (
-//             <div>
-//                 <Link to={'/home'} key={'Home'} name={'Home'}/>
-//
-//                 <h3>{item.id}</h3>
-//                 <p>{item.type}</p>
-//             </div>
-//         )
-//     };
-// }
-
 
 pages = {
+    '/': {
+        component: <Home mergeState={store.mergeState.bind(store)}/>,
+        load: function () {
+            changeUrl('/home');
+            history[window.location.href] = this.component;
+            console.log('history', history);
+            return this.component;
+        }
+    },
     '/home': {
-        component: <Home mergeState={store.mergeState.bind(store)}/>
+        component: <Home mergeState={store.mergeState.bind(store)}/>,
+        load: function () {
+            // history.push(window.location.href, this.component);
+            history[window.location.href] = this.component;
+            console.log('history', history);
+            return this.component;
+        }
     },
     '/home/:id': {
-        component: <Detail mergeState={store.mergeState.bind(store)}/>
+        component: <Detail mergeState={store.mergeState.bind(store)}/>,
+        load: function (path) {
+            console.log('load path: ' + path)
+            changeUrl(path);
+            history[window.location.href] = this.component;
+            console.log('history', history);
+            return this.component;
+        }
     },
-
 };
 
-function changeUrl ( path ) {
-    window.history.pushState(null, null, path);
-}
 
 class App extends Component {
     constructor(props) {
@@ -242,8 +158,21 @@ class App extends Component {
         this.state = {};
     }
 
+    getData = function (callback) {
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('GET', url, false);
+        xhr.send();
+
+        if (xhr.status !== 200) {
+            callback({message: xhr.status + ': ' + xhr.statusText});
+        } else {
+            callback(null, xhr.responseText);
+        }
+    };
+
     componentDidMount() {
-        getData((error, respondData) => {
+        this.getData((error, respondData) => {
             if (!error) {
                 this.setState({
                     isLoaded: true,
@@ -259,14 +188,13 @@ class App extends Component {
     }
 
     render() {
+        const {error, isLoaded} = this.state;
+
         let pathname = window.location.pathname,
             page = <p>Page not found...</p>,
             hash = window.location.hash,
             pageName;
 
-        const {error, isLoaded} = this.state;
-
-        // console.log(window.location)
         store.mergeState.call(store, this.state);
 
         if (error) {
@@ -276,25 +204,21 @@ class App extends Component {
         } else {
             pathname = (pathname === '/') ? pathname : pathname.replace(/\/$/, "");
 
-            console.log('pathname: ', pathname);
-            console.log('hash: ', hash);
-
-            if (pathname === '/home' && hash) {
-                changeUrl(pathname + '/' + hash);
-                page = pages['/home/:id'].component;
-            } else {
-                if (pathname === '/') {
-                    pageName = '/home';
-                    changeUrl(pageName);
-                    page = pages[pageName].component;
-                } else if (pathname in pages) {
-                    page = pages[pathname].component;
-                }
+            if (pathname in pages) {
+                pageName = hash ? pathname + '/:id' : pathname;
+                page = pages[pageName].load(pathname + '/' + hash);
             }
         }
 
         return (page);
     }
 }
+
+window.onpopstate = function(event) {
+    // console.log("document.location.href: ", document.location.href, ", state: " + JSON.stringify(event.state));
+    // console.log('history[document.location.href]: ' + history[window.location.href]);
+    ReactDOM.render(history[window.location.href], document.getElementById('root'));
+};
+
 
 export default App;
