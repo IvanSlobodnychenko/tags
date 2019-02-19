306186720c2a47af8a2f0c7fd7b8623892918c24
ivan <vanyaslobodnich@gmail.com>
2019-02-19 06:13:09 +0200

    old implementation

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import React, {Component} from 'react';
-// import logo from './logo.svg';
-import './App.css';
 import ReactDOM from "react-dom";
-// import Tag from './components/tag';
 import Store from './store';
 
 
-var url = 'https://gist.githubusercontent.com/snownoop/e6ca04705cf03cbe6ef9beaf16a306ab/raw/07906333730ca961a8091a8c16b05d26a8ee7cd9/Tags%2520Cloud%2520Data',
+let url = 'https://gist.githubusercontent.com/snownoop/e6ca04705cf03cbe6ef9beaf16a306ab/raw/07906333730ca961a8091a8c16b05d26a8ee7cd9/Tags%2520Cloud%2520Data',
     store = new Store(),
+    history = [],
+    index = -1,
+    historyUsed = false,
     pages;
-
-
-function getData(callback) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.open('GET', url, false);
-
-    xhr.send();
-
-    if (xhr.status !== 200) {
-        // console.log(xhr.status + ': ' + xhr.statusText);
-        callback({message: xhr.status + ': ' + xhr.statusText});
-    } else {
-        // console.log(xhr.responseText);
-        callback(null, xhr.responseText);
-    }
-}
 
 
 class Link extends Component {
@@ -37,53 +20,39 @@ class Link extends Component {
         }
     }
 
-
     route = (link) => {
-        console.log('call link', link);
         let id = this.props.id ? '/#' + this.props.id : '';
 
-
-        // if (link === '/home') {
-        //     window.history.pushState(null, null, '/home');
-        //     ReactDOM.render(pages['/home'].component, document.getElementById('root'));
-        // } else { // add id
-        //     window.history.pushState(null, null, '/home/#1751295897__Odessa');
-        //     ReactDOM.render(pages['/detail'].component, document.getElementById('root'));
-        // }
-
-        if ( link in pages ) {
-            window.history.pushState(null, null, '/home' + id);
-            ReactDOM.render(pages[link].component, document.getElementById('root'));
+        if (link in pages) {
+            ReactDOM.render(pages[link].load('/home' + id), document.getElementById('root'));
         }
     };
 
     render() {
         return (
-            <button onClick={() => {
-                this.route(this.state.link)
-            }} key={this.state.name}>
+            <a href="false"  onClick={(e) => { e.preventDefault(); this.route(this.state.link)}} key={this.state.name}>
                 {this.state.name}
-            </button>
+            </a>
         );
     }
 }
 
-class Home extends Component {
-    constructor(props) {
-        super(props);
-    }
 
+class Home extends Component {
     render() {
         let {items} = store.getState();
 
         return (
             <div>
-                {/*<Link to={'/detail'} key={'Detail'} name={'Detail'}/>*/}
-                <h3>Home</h3>
+                <h3>Home Page</h3>
 
                 {items.map(item => (
-                    <Link to={'/detail'} id={item.id} key={item.label} name={item.label}/>
-                    // <Link to={'/home/#' + item.id} key={item.label} name={item.label}/>
+                    <button
+                        className={'tag-button'}
+                        style={{fontSize: item.sentimentScore > 1 ? item.sentimentScore * 0.4 : 12}}
+                        key={item.id}>
+                        <Link to={'/home/:id'} id={item.id} key={item.label} name={item.label}/>
+                    </button>
                 ))}
             </div>
         )
@@ -92,15 +61,10 @@ class Home extends Component {
 
 
 class Detail extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     getItemById = (id, items) => {
         let index = 0;
 
         for (index; index < items.length; index++) {
-            console.log((items[index].id === id), items[index].id, id);
             if (items[index].id === id) {
                 return items[index];
             }
@@ -124,9 +88,24 @@ class Detail extends Component {
             return (
                 <div>
                     <Link to={'/home'} key={'Home'} name={'Home'}/>
-                    <h3>Detail</h3>
-                    <p>{item.id}</p>
-                    <p>{item.label}</p>
+                    <h3>Detail page</h3>
+
+                    <ul>
+                        <li key={item.label}>label: {item.label}</li>
+                        <li key={'total'} >total props: {Object.keys(item).length - 1}</li>
+                    </ul>
+
+                    <ul>
+                        {Object.keys(item.sentiment).map(key => (
+                            <li key={key} >{key}: {item.sentiment[key]}</li>
+                        ))}
+                    </ul>
+
+                    <ul>
+                        {Object.keys(item.pageType).map(key => (
+                            <li key={key}>{key}: {item.pageType[key]}</li>
+                        ))}
+                    </ul>
                 </div>
             )
         }
@@ -134,111 +113,85 @@ class Detail extends Component {
         return (
             <div>
                 <Link to={'/home'} key={'Home'} name={'Home'}/>
-                <h3>Detail</h3>
+                <h3>Detail Page</h3>
                 <p>nothing to show</p>
             </div>
         )
     };
 }
 
-// class Home extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             // hash: window.location.hash
-//         };
-//     }
-//
-//
-//     getItemById = (id, items) => {
-//         let index = 0;
-//
-//         for (index; index < items.length; index++) {
-//             console.log((items[index].id === id), items[index].id, id);
-//             if (items[index].id === id) {
-//                 return items[index];
-//             }
-//         }
-//
-//         return null;
-//     };
-//
-//     render() {
-//         let {items} = store.getState(),
-//             hash = window.location.hash,
-//             item, id;
-//
-//         console.log('hash: ' + hash);
-//
-//         if (hash) {
-//             // get rid of hash symbol
-//             id = hash.slice(1);
-//
-//             item = this.getItemById(id, items);
-//
-//             console.log('item: ', item);
-//
-//             if (item) {
-//                 console.log('load detail');
-//                 return <Detail mergeState={store.mergeState.bind(store)} id={id} item={item}/>
-//             }
-//         }
-//
-//         console.log('load home');
-//
-//         return (
-//             <div>
-//                 {items.map(item => (
-//                     <Link to={'/home/#' + item.id} key={item.label} name={item.label}/>
-//                 ))}
-//             </div>
-//         );
-//     }
-// }
-//
-//
-// class Detail extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             id: props.id,
-//             item: props.item
-//         }
-//     }
-//
-//     render() {
-//         let item = this.state.item;
-//
-//         return (
-//             <div>
-//                 <Link to={'/home'} key={'Home'} name={'Home'}/>
-//
-//                 <h3>{item.id}</h3>
-//                 <p>{item.type}</p>
-//             </div>
-//         )
-//     };
-// }
 
+function loadHandle (path, component ) {
+    // clean history more then current page index
+    if ( historyUsed ) {
+        history.splice(index + 1);
+        historyUsed = false;
+    }
+
+    // change url without reloading
+    window.history.pushState(null, null, path);
+
+    // fill the history
+    history.push({
+        href: window.location.href,
+        component: component
+    });
+
+    ++index;
+
+    // console.log('currentIndex: ' + index, history);
+}
 
 pages = {
-    '/home': {
-        component: <Home mergeState={store.mergeState.bind(store)}/>
-    },
-    '/detail': {
-        component: <Detail mergeState={store.mergeState.bind(store)}/>
-    },
+    '/': {
+        component: <Home mergeState={store.mergeState.bind(store)}/>,
+        load: function () {
+            loadHandle('/home', this.component);
 
+            return this.component;
+        }
+    },
+    '/home': {
+        component: <Home mergeState={store.mergeState.bind(store)}/>,
+        load: function () {
+            loadHandle('/home', this.component);
+
+            return this.component;
+        }
+    },
+    '/home/:id': {
+        component: <Detail mergeState={store.mergeState.bind(store)}/>,
+        load: function (path) {
+            loadHandle(path, this.component);
+
+            return this.component;
+        }
+    },
 };
 
+
+// main component
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {};
     }
 
+    getData = function (callback) {
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', url, false);
+        xhr.send();
+
+        if (xhr.status !== 200) {
+            callback({message: xhr.status + ': ' + xhr.statusText});
+        } else {
+            callback(null, xhr.responseText);
+        }
+    };
+
     componentDidMount() {
-        getData((error, respondData) => {
+        this.getData((error, respondData) => {
             if (!error) {
                 this.setState({
                     isLoaded: true,
@@ -254,32 +207,49 @@ class App extends Component {
     }
 
     render() {
-        let pathname = window.location.pathname,
-            page = <p>Page not found...</p>;
-
         const {error, isLoaded} = this.state;
 
-        store.mergeState.call(store, this.state);
+        let pathname = window.location.pathname,
+            hash = window.location.hash,
+            // default view
+            page = <p>Page not found...</p>,
+            pageName;
 
-        pathname = (pathname === '/') ? '/home' : pathname.replace(/\/$/, "");
+        store.mergeState.call(store, this.state);
 
         if (error) {
             return <div>Error: {error.message}</div>;
         } else if (!isLoaded) {
             return <div>Loading...</div>;
         } else {
+            // get rid of last slash
+            pathname = (pathname === '/') ? pathname : pathname.replace(/\/$/, "");
 
             if (pathname in pages) {
-                page = pages[pathname].component;
-            } else if (pathname === '/') {
-                // todo: redirect from '/' to '/home'
-                // window.history.pushState(null, null, '/home');
-                page = pages['/home'].component;
+                // change page name for pages with #id
+                pageName = hash ? pathname + '/:id' : pathname;
+                page = pages[pageName].load(pathname + '/' + hash);
             }
         }
 
         return (page);
     }
 }
+
+
+window.onpopstate = function () {
+    let href = document.location.href;
+
+    // if moving forward
+    if (history[index + 1] && href === history[index + 1].href) {
+        ReactDOM.render(history[++index].component, document.getElementById('root'));
+    // if moving back
+    } else if (history[index - 1] && href === history[index - 1].href) {
+        ReactDOM.render(history[--index].component, document.getElementById('root'));
+    }
+
+    historyUsed = true;
+};
+
 
 export default App;
